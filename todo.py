23606a7daaa97ec433c9da08cecf205e6cf1fc28
Stephen McMahon <stephen@slmcmahon.com>
2016-10-24 22:21:42 +0400

    deleted todo.py

--- todo.py
+++
@@ -1,9 +0,0 @@
-from google.appengine.ext import ndb
-
-class TodoItem(ndb.Model):
-    creator = ndb.UserProperty(indexed=True)
-    text = ndb.StringProperty(indexed=False)
-    active = ndb.BooleanProperty(default=True, indexed=True)
-    category = ndb.StringProperty(indexed=True)
-    created = ndb.DateTimeProperty(auto_now_add=True)
-    closed_date = ndb.DateProperty(auto_now_add=False)
