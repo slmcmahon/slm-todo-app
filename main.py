3ed950cf6278f7e3b2cfc9392ca2fcd3fca462c3
Stephen McMahon <stephen@slmcmahon.com>
2016-10-25 08:22:17 +0400

    Added due date property to todo items and added ability to show closed items.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, redirect, request
 from google.appengine.api import users
 from todomanager import TodoManager
+import time
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
+DEBUG = True
 
 
 @app.route('/')
@@ -11,10 +14,17 @@ def get_todo_items():
     if not user:
         return redirect(users.create_login_url('/'))
 
-    mgr = TodoManager()
-    todos = mgr.get_todo_items(user)
+    show_closed = False if not request.args.get('closed') == 'true' else True
 
-    return render_template('todolist.html', data={'todos': todos})
+    # set the due date to 1 day in the future by default
+    duedate = datetime.today() + timedelta(days=1)
+    values = {
+        'now': str(duedate)[:16],
+        'closed': show_closed,
+        'todos': TodoManager.get_todo_items(user, show_closed)
+    }
+
+    return render_template('todolist.html', data=values)
 
 
 @app.route('/addtodo', methods=['POST'])
@@ -23,8 +33,20 @@ def add_todo():
     if not user:
         return redirect(users.create_login_url('/addtodo'))
 
-    text = request.form["todotext"]
-    TodoManager.add_todo(user, text)
+    text = request.form['todotext']
+    due = request.form['duedate']
+
+    due_date = None
+
+    if due:
+        due_date = datetime.strptime(due, "%Y-%m-%d %H:%M")
+
+
+    TodoManager.add_todo(user, text, due=due_date)
+
+    if DEBUG:
+        time.sleep(.5)
+
     return redirect('/')
 
 
@@ -35,6 +57,10 @@ def complete_todo(key):
         return redirect(users.create_login_url('completetodo/{}'.format(key)))
 
     TodoManager.close_todo(key)
+
+    if DEBUG:
+        time.sleep(.5)
+
     return redirect('/')
 
 
@@ -45,6 +71,10 @@ def delete_todo(key):
         return redirect(users.create_login_url('completetodo/{}'.format(key)))
 
     TodoManager.delete_todo(key)
+
+    if DEBUG:
+        time.sleep(.5)
+
     return redirect('/')
 
 if __name__ == '__main__':
--- todomanager.py
+++ todomanager.py
@@ -8,22 +8,29 @@ class TodoItem(ndb.Model):
     active = ndb.BooleanProperty(default=True, indexed=True)
     category = ndb.StringProperty(indexed=True)
     created = ndb.DateTimeProperty(auto_now_add=True)
+    due_date = ndb.DateTimeProperty(auto_now_add=False)
     closed_date = ndb.DateTimeProperty(auto_now_add=False)
 
 
 class TodoManager():
 
     @staticmethod
-    def add_todo(user, text, category=None):
+    def add_todo(user, text, category=None, due=None):
         todo = TodoItem()
         todo.creator = user
         todo.text = text
         todo.category = category if category else 'General'
+        if due:
+            todo.due_date = due
         todo.put()
 
     @staticmethod
-    def get_todo_items(user):
-        qry = TodoItem.query(TodoItem.creator == user and TodoItem.active == True)
+    def get_todo_items(user, show_closed):
+        if show_closed:
+            qry = TodoItem.query(TodoItem.creator == user)
+        else:
+            qry = TodoItem.query(TodoItem.creator == user and TodoItem.active == True)
+
         return qry.fetch(limit=None)
 
     @staticmethod
